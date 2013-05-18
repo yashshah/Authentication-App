ad3ebea5ae347235e7795d02bd271da9007532b6
Yash Shah <mail@yashshah.com>
2013-05-18 18:23:02 +0530

    Add proper notification when signing in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,11 @@ end
  def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url, notice: t('.signed_in')
+    redirect_to root_url, notice: "Successfully signed in"
   end
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: t('.signed_out')
+    redirect_to root_url, notice: "Successfully signed out"
   end
 end
